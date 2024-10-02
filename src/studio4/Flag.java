4842227c33f4cda95c927885c8b170028b7f7cf0
arpanpatel <arpanpatel@localhost>
2024-10-02 15:43:04 -0500

    dfg

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,33 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		Color pissYellow = new Color(222,198,44);
+		Color uglySeahawkGreen = new Color (0, 255, 68);
+		Color normalPink = new Color(255, 0, 179);
+		
+		StdDraw.setPenColor(pissYellow); 
+		StdDraw.filledRectangle(0, 1, 1, 1);
+		
+		StdDraw.setPenColor(uglySeahawkGreen); 
+		StdDraw.filledCircle(0.5, 0.75, 0.1);
+		
+		StdDraw.setPenColor(normalPink);
+		StdDraw.filledRectangle(0.5, 0.75, 0.05, 0.05);
+		
+		StdDraw.setPenColor(0,0,0);
+		double [] x = {0.4, 0.5, 0.6};
+		double [] y = {0.5, 0.6, 0.5};
+		StdDraw.filledPolygon(x, y);
+		
+		StdDraw.setPenColor(50,50,200);
+		double [] a = {0.01, 0.2, 0.3, 0.4, 0.5, 0.55};
+		double [] b = {0.6, 0.1, 0.2, 0.3, 0.4, 0.5}; 
+		StdDraw.filledPolygon(a, b);
+
+		
+		
+		
+		
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +21,29 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		
+		String shape = in.next();
+		int R = in.nextInt();
+		int G = in.nextInt();
+		int B = in.nextInt();
+		
+		boolean filled = in.nextBoolean();
+		
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeight = in.nextDouble();
+		
+	
+		Color black = new Color(R, G, B);
+
+if (!filled) {
+	
+StdDraw.filledRectangle( x, y, halfWidth, halfHeight);
+
+}
+
+
+
 	}
 }
